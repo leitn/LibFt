bf83d5196ff2504e6463598b2a7dd5e7e6fcb4ee
letnitan <leila.tnitan.pro@gmail.com>
2022-11-10 18:58:32 +0100

    tolower

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: letnitan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/10 18:27:23 by letnitan          #+#    #+#             */
-/*   Updated: 2022/11/10 18:27:27 by letnitan         ###   ########.fr       */
+/*   Created: 2022/11/10 18:40:57 by letnitan          #+#    #+#             */
+/*   Updated: 2022/11/10 18:41:01 by letnitan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_lowercase(char c)
+int	ft_upperercase(char c)
 {
-	if ((c >= 'a' && c <= 'z'))
+	if ((c >= 'A' && c <= 'Z'))
 			return (0);
 	else
 	        return (1);
 }
 
-int ft_toupper(int c)
+int ft_tolower(int c)
 {
-    if (ft_lowercase(c) == 0)
-        return (c - 32);
+    if (ft_uppercase(c) == 0)
+        return (c + 32);
     else
         return c;
 }
