b68228d27c31b36711e7d0d9a853eb14cd988318
letnitan <leila.tnitan.pro@gmail.com>
2022-11-10 11:29:24 +0100

    bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -13,4 +13,14 @@
 #include <strings.h>
 
 void ft_bzero(void *s, size_t n)
-{}
+{    
+    int i;
+    unsigned char *s = str;
+
+    i = 0;
+    while(i < n)
+    {
+        s[i] = 0;
+        i++;
+    }
+}
