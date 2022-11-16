688abcaca816b90d64ef053f1d16dd91ab7ce9e7
letnitan <leila.tnitan.pro@gmail.com>
2022-11-16 12:37:40 +0100

    le probleme venait du fait que j'avais mis le test de n==0 en debut de code, il fallait le deplacer sous la boucle while

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,13 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (n == 0)
-		return (0);
 	while (n > 0 && (*s1 == *s2) && (*s1))
 	{
 		s1++;
 		s2++;
 		n--;
 	}
-	return (*s1 - *s2);
+	if (n == 0)
+		return (0);
+	else
+		return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
