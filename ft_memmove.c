00a061cf907a02f40c9f0a3ac79d147ddb34fdcb
letnitan <leila.tnitan.pro@gmail.com>
2022-11-16 12:07:04 +0100

    ft_memmove.c passe la warmachine

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,4 +12,31 @@
 
 #include "libft.h"
 
-void *memmove(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*s;
+	char	*d;
+	size_t	i;
+
+	i = 0;
+	d = (char *)dest;
+	s = (char *)src;
+	if (!dest && !src)
+		return (NULL);
+	if (d > s)
+	{
+		while (n-- > 0)
+		{
+			d[n] = s[n];
+		}
+	}
+	if ((ft_strlen(s) - ft_strlen(d)) >= n)
+	{
+		while (i < n && dest != src && d != s)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
+}
