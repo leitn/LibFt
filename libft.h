43b3feff8394f7f43377c0cf9c3772d429707ed6
letnitan <leila.tnitan.pro@gmail.com>
2022-11-21 18:20:47 +0100

    libft et makefile

--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@ int	ft_isprint (int c);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
 int	ft_atoi(const char *str);	
+char	*ft_itoa(int n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_bzero(void *str, size_t n);
