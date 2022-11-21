f5187febee92b2e433d34d267a8fe7a958f4d45b
letnitan <leila.tnitan.pro@gmail.com>
2022-11-21 19:04:27 +0100

    strmapi merci caro

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,3 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letnitan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 19:00:56 by letnitan          #+#    #+#             */
+/*   Updated: 2022/11/21 19:00:59 by letnitan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*newstr;
+
+	newstr = malloc(sizeof(char) * (strlen(s) + 1));
+	if (newstr == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		newstr[i] = f(i, s[i]);
+		i++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
+}
