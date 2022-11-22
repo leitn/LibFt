144e8b937317c85b36d0d3db1cd490767f82d9a7
letnitan <leila.tnitan.pro@gmail.com>
2022-11-22 12:03:40 +0100

    ft_putendl_fd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,3 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letnitan <letnitan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 11:56:48 by letnitan          #+#    #+#             */
+/*   Updated: 2022/11/22 12:03:04 by letnitan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void ft_putendl_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd)
+{
+	int		i;
+	char	x;
+
+	i = 0;
+	x = 10;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, &x, 1);
+}
