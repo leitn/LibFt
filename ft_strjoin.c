047799cad687a9494045850403313bd129c354d8
letnitan <leila.tnitan.pro@gmail.com>
2022-11-22 16:52:17 +0100

    strjoin had too many lines

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: letnitan <letnitan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 19:00:43 by letnitan          #+#    #+#             */
-/*   Updated: 2022/11/22 14:56:29 by letnitan         ###   ########.fr       */
+/*   Updated: 2022/11/22 16:51:08 by letnitan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		i++;
 	}
 	while (s2[j] && j <= ft_strlen(s2))
-	{
-		str[i] = s2[j];
-		j++;
-		i++;
-	}
+		str[i++] = s2[j++];
 	str[lentotal] = '\0';
 	return (str);
 }
