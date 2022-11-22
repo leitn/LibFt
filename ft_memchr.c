4a2b0f011e7d2e2f0f436c2944a0c516a8dd8f82
letnitan <leila.tnitan.pro@gmail.com>
2022-11-22 17:06:50 +0100

    je verifie trop de conditions

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: letnitan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: letnitan <letnitan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 02:14:10 by letnitan          #+#    #+#             */
-/*   Updated: 2022/11/12 02:14:14 by letnitan         ###   ########.fr       */
+/*   Updated: 2022/11/22 17:05:45 by letnitan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,12 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	size;
 
-	if (n == 0)
-		return (NULL);
 	size = 0;
-	while ((size < n) && ((char *)s))
+	while (size < n)
 	{
 		if (*((char *)s + size) == (char )c)
 			return ((void *)s + size);
-		else
-		{
-			size++;
-		}
+		size++;
 	}
 	return (NULL);
 }
