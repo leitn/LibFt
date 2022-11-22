890b319e63a79a1efd3cc030e5d7c52f58feee88
letnitan <leila.tnitan.pro@gmail.com>
2022-11-22 16:10:21 +0100

    strlcat updatee

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,32 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: letnitan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: letnitan <letnitan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 16:20:10 by letnitan          #+#    #+#             */
-/*   Updated: 2022/11/10 16:20:13 by letnitan         ###   ########.fr       */
+/*   Updated: 2022/11/22 16:07:44 by letnitan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <bsd/string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	srclen;
 	size_t	dstlen;
 	size_t	i;
 
-	srclen = ft_strlen(src);
-	dstlen = ft_strlen(dst);
 	i = 0;
-	if (size == 0 || size < dstlen)
-		return (srclen + size);
-	while (src[i] && (dstlen + i < size -1))
+	if (dst == NULL)
+		dstlen = 0;
+	else
+		dstlen = ft_strlen(dst);
+	if (size < dstlen + 1)
+		return (size + ft_strlen(src));
+	if (size > dstlen + 1)
 	{
-		dst[dstlen + i] = src[i];
-		i++;
+		while (src[i] && (dstlen + i < size - 1))
+		{
+			dst[dstlen + i] = src[i];
+			i++;
+		}
 	}
 	dst[dstlen + i] = '\0';
-	return (srclen + dstlen);
+	return (ft_strlen(src) + dstlen);
 }
