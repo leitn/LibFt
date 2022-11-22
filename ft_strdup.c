9131574a183dee31bd2d64a0f0859c4ad356debe
letnitan <leila.tnitan.pro@gmail.com>
2022-11-22 15:22:43 +0100

    all the rest

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: letnitan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: letnitan <letnitan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 11:41:15 by letnitan          #+#    #+#             */
-/*   Updated: 2022/11/15 11:41:19 by letnitan         ###   ########.fr       */
+/*   Updated: 2022/11/22 12:44:14 by letnitan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strdup(const char *s)
 
 	len = ft_strlen((char *)s);
 	strduplicate = ft_calloc(len + 1, sizeof(char));
+	if (!(strduplicate))
+		return (NULL);
 	ft_memcpy(strduplicate, s, len);
 	return (strduplicate);
 }
